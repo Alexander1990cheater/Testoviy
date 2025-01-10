1e2282da237e0f2e0c214cd6dabaa7a80951e88f
Alexander1990cheater <139058903+Alexander1990cheater@users.noreply.github.com>
2025-01-10 13:09:35 +0300

    Обновление от 10.01.2025

--- src/test/java/ru/academits/LessonSel.java
+++ src/test/java/ru/academits/LessonSel.java
@@ -1,17 +1,37 @@
 package ru.academits;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LessonSel {
-    @Test
-    public void simpleTest(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+public class LessonSelTest {
+    private WebDriver driver;
 
-        driver.get("https://market.yandex.ru/?from=dist_bookmarks&win=596&clid=2721055-61");
-        driver.quit();
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        String url = "https://market.yandex.ru/?from=dist_bookmarks&win=596&clid=2721055-61";
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testYandexMarketTitle() {
+        String title = driver.getTitle();
+        System.out.println("Page title: " + title);
+        Assertions.assertEquals("Интернет-магазин Яндекс Маркет — покупки с быстрой доставкой", title);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit(); // Закрытие браузера после каждого теста
+        }
     }
 }
